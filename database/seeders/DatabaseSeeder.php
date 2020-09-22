259321e6bd0c68916f7056742c672748d36de3c3
Renandatta Rooziq <renandattarooziq@gmail.com>
2020-09-22 20:07:43 +0700

    user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\ModulUtama\User;
+use App\Models\ModulUtama\UserLevel;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user_level = UserLevel::create([
+            'nama' => 'Superadmin',
+            'keterangan' => 'Top level user'
+        ]);
+        $user = User::create([
+            'user_level_id' => $user_level->id,
+            'nama' => 'Superadmin',
+            'email' => 'super@admin.com',
+        ]);
+        $user->password = Hash::make('4rt1s4n');
+        $user->save();
     }
 }
