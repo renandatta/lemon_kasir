8c166c964d1102913c8f1092bb81ba99dc9015db
Renandatta Rooziq <renandattarooziq@gmail.com>
2020-09-22 20:04:20 +0700

    basic route

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('image/{folder}/{filename}', function ($folder,$filename){
+    $path = storage_path('app/' . $folder . '/' . $filename);
+    if (!File::exists($path)) {
+        abort(404);
+    }
+    $file = File::get($path);
+    $type = File::mimeType($path);
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+    return $response;
+});
+
 
 Route::get('/', function () {
     return view('welcome');
