9b25ee2a9244ca496d37568896226cd77a12d9e7
Renandatta Rooziq <renandattarooziq@gmail.com>
2020-09-28 21:04:29 +0700

    sub hak akses dari fitur

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,21 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Session;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function additional_action($action)
+    {
+        $hak_akses_fitur = Session::get('hak_akses_fitur') ?? array();
+        foreach ($hak_akses_fitur as $fitur) {
+            if ($fitur->flag_akses == true) {
+                $fitur_url = explode(".", $fitur->url);
+                array_push($action, end($fitur_url));
+            }
+        }
+        return $action;
+    }
 }
--- app/Http/Middleware/HakAksesMiddleware.php
+++ app/Http/Middleware/HakAksesMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Http\Controllers\Pengaturan\FiturProgramRepository;
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class HakAksesMiddleware
 {
@@ -16,12 +17,18 @@ class HakAksesMiddleware
 
     public function handle($request, Closure $next)
     {
+        $user_level_id = Auth::user()->user_level_id;
         $blocked = true;
         $method_post = $request->isMethod('post');
         $exception = array('profil', 'ubah_password', '/');
         $current_route = $request->route()->getName();
+        $fitur = $this->fiturProgram->find($current_route, 'url');
+        if (!empty($fitur)) {
+            $hak_akses_fitur = $this->fiturProgram->nested_data($fitur->kode, $user_level_id);
+            Session::put('hak_akses_fitur', $hak_akses_fitur);
+        }
 
-        $fitur_program = $this->fiturProgram->nested_data('#', Auth::user()->user_level_id);
+        $fitur_program = $this->fiturProgram->nested_data('#', $user_level_id);
         foreach ($fitur_program as $fitur) {
             foreach ($fitur->children as $menu)
                 if ($menu->url != null && strpos($current_route, $menu->url) !== false && $menu->flag_akses == true)
