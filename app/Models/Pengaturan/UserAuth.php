27e21559177ae44fd79f7670ce4a955eb06b791f
Renandatta Rooziq <renandattarooziq@gmail.com>
2020-09-29 22:36:25 +0700

    fix user auth

--- app/Models/Pengaturan/UserAuth.php
+++ app/Models/Pengaturan/UserAuth.php
@@ -15,7 +15,6 @@ class UserAuth extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id', 'id')
-            ->with(['siswa', 'guru']);
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 }
