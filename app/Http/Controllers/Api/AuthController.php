a97f1a21261d6fd54c3633a327f5761a2f2bb371
Renandatta Rooziq <renandattarooziq@gmail.com>
2020-09-29 21:01:00 +0700

    register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Master\LisensiProfilRepository;
+use App\Http\Controllers\Master\ProfilRepository;
+use App\Http\Controllers\Master\UserProfilRepository;
 use App\Http\Controllers\Pengaturan\UserRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
-    protected $user;
-    public function __construct(UserRepository $user)
+    protected $user, $profil, $userProfil, $lisensiProfil;
+    public function __construct(UserRepository $user, ProfilRepository $profil,
+                                UserProfilRepository $userProfil, LisensiProfilRepository $lisensiProfil)
     {
         $this->user = $user;
+        $this->profil = $profil;
+        $this->userProfil = $userProfil;
+        $this->lisensiProfil = $lisensiProfil;
     }
 
     public function login(Request $request)
@@ -30,5 +37,39 @@ class AuthController extends Controller
         $token = $request->input('_token');
         $this->user->logout($token);
         return response()->json(['success' => $token]);
+    }
+
+    public function register(Request $request)
+    {
+        if (
+            !$request->has('nama_profil') || !$request->has('notelp') ||
+            !$request->has('alamat') || !$request->has('kota') ||
+            !$request->has('email') || !$request->has('password') ||
+            !$request->has('nama_user') || !$request->has('lisensi_id') || !$request->has('harga')
+        ) return abort(404);
+
+        $profil_req = new Request($request->only('notelp', 'alamat', 'kota'));
+        $profil_req->merge(['nama' => $request->input('nama_profil')]);
+        $profil = $this->profil->save($profil_req);
+
+        $user_req = new Request($request->only('email', 'password'));
+        $user_req->merge(['nama' => $request->input('nama_user')]);
+        $user = $this->user->save($user_req);
+
+        $user_profil_req = new Request();
+        $user_profil_req->merge(['profil_id' => $profil->id]);
+        $user_profil_req->merge(['user_id' => $user->id]);
+        $this->userProfil->save($user_profil_req);
+
+        $lisensi_profil_req = new Request($request->only('lisensi_id', 'harga'));
+        $lisensi_profil_req->merge(['profil_id' => $profil->id]);
+        $lisensi_profil_req->merge(['no_lisensi' => $this->lisensiProfil->nomor_otomatis()]);
+        $lisensi_profil_req->merge(['berlaku_dari' => date('Y-m-d')]);
+        $this->lisensiProfil->save($lisensi_profil_req);
+
+        $login_req = new Request($request->only('email', 'password'));
+        $login_req->merge(['_token' => Str::random(32)]);
+        $user = $this->user->login(new Request($request->only(['email', 'password', '_token'])));
+        return response()->json(['success' => $user]);
     }
 }
