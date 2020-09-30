5685733d4a238f48ff81b0d7f358aabd6f74462d
Renandatta Rooziq <renandattarooziq@gmail.com>
2020-09-30 22:34:44 +0700

    penjualan repository

---
+++ app/Http/Controllers/Penjualan/PenjualanRepository.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Penjualan;
+
+use App\Models\Penjualan\DetailPenjualan;
+use App\Models\Penjualan\Penjualan;
+use Illuminate\Http\Request;
+
+class PenjualanRepository
+{
+    protected $penjualan, $detailPenjualan;
+    public function __construct(Penjualan $penjualan, DetailPenjualan $detailPenjualan)
+    {
+        $this->penjualan = $penjualan;
+        $this->detailPenjualan = $detailPenjualan;
+    }
+
+    public function search(Request $request)
+    {
+        $penjualan = $this->penjualan;
+
+        $profil_id = $request->input('profil_id') ?? '';
+        if ($profil_id != '') $penjualan = $penjualan->where('profil_id', $profil_id);
+
+        $no_penjualan = $request->input('no_penjualan') ?? '';
+        if ($no_penjualan != '') $penjualan = $penjualan->where('no_penjualan', $no_penjualan);
+
+        $tanggal = $request->input('tanggal') ?? '';
+        if ($tanggal != '') $penjualan = $penjualan->whereDate('created_at', $tanggal);
+
+        if ($request->has('paginate')) return $penjualan->paginate($request->input('paginate'));
+        return $penjualan->get();
+    }
+
+    public function search_detail(Request $request)
+    {
+        $detail = $this->detailPenjualan;
+
+        $penjualan_id = $request->input('penjualan_id') ?? '';
+        if ($penjualan_id != '') $detail = $detail->where('penjualan_id', $penjualan_id);
+
+        $produk_id = $request->input('produk_id') ?? '';
+        if ($produk_id != '') $detail = $detail->where('produk_id', $produk_id);
+
+        $jumlah = $request->input('jumlah') ?? '';
+        if ($jumlah != '') $detail = $detail->where('jumlah', $jumlah);
+
+        $tanggal = $request->input('tanggal') ?? '';
+        if ($tanggal != '') $detail = $detail->whereDate('tanggal', $tanggal);
+
+        $jumlah_dari = $request->input('jumlah_dari') ?? '';
+        if ($jumlah_dari != '') $detail = $detail->where('jumlah', '>=', $jumlah_dari);
+
+        $jumlah_sampai = $request->input('jumlah_sampai') ?? '';
+        if ($jumlah_sampai != '') $detail = $detail->where('jumlah', '>=', $jumlah_sampai);
+
+        $tanggal_dari = $request->input('tanggal_dari') ?? '';
+        if ($tanggal_dari != '') $detail = $detail->whereDate('tanggal', '>=', $tanggal_dari);
+
+        $tanggal_sampai = $request->input('tanggal_sampai') ?? '';
+        if ($tanggal_sampai != '') $detail = $detail->whereDate('tanggal', '>=', $tanggal_sampai);
+
+        if ($request->has('paginate')) return $detail->paginate($request->input('paginate'));
+        return $detail->get();
+    }
+
+    public function find($value, $column = 'id')
+    {
+        return $this->penjualan->where($column, '=', $value)->first();
+    }
+
+    public function find_detail($value, $column = 'id')
+    {
+        return $this->detailPenjualan->where($column, '=', $value)->first();
+    }
+
+    public function save(Request $request)
+    {
+        return $this->penjualan->save($request->only('profil_id', 'no_penjualan'));
+    }
+
+    public function save_detail(Request $request)
+    {
+        if ($request->has('id')) {
+            $detail = $this->detailPenjualan->save($request->only('penjualan_id', 'produk_id', 'jumlah', 'harga'));
+        } else {
+            $detail = $this->detailPenjualan->find($request->input('id'));
+            $detail->update($request->only('jumlah'));
+        }
+        return $detail;
+    }
+
+    public function delete($id)
+    {
+        $penjualan = $this->penjualan->find($id);
+        $penjualan->delete();
+        return $penjualan;
+    }
+
+    public function delete_detail($id)
+    {
+        $detail = $this->detailPenjualan->find($id);
+        $detail->delete();
+        return $detail;
+    }
+
+    public function nomor_otomatis()
+    {
+        return date('Y') . '/' . numberToRoman(date('n')) . '/' . date('d') . '/' . strval(mt_rand(111111, 999999));
+    }
+}
