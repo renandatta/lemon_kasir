908745306d2214e3fcd3baff7f758ab688716b33
Renandatta Rooziq <renandattarooziq@gmail.com>
2020-10-19 18:36:24 +0700

    fake penjualan

--- app/Http/Controllers/Kasir/PenjualanKasirController.php
+++ app/Http/Controllers/Kasir/PenjualanKasirController.php
@@ -5,19 +5,21 @@ namespace App\Http\Controllers\Kasir;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Master\ProfilRepository;
 use App\Http\Controllers\Penjualan\PenjualanRepository;
+use App\Http\Controllers\Produk\ProdukRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
 class PenjualanKasirController extends Controller
 {
-    protected $breadcrumbs, $profil, $penjualan;
-    public function __construct(ProfilRepository $profil, PenjualanRepository $penjualan)
+    protected $breadcrumbs, $profil, $penjualan, $produk;
+    public function __construct(ProfilRepository $profil, PenjualanRepository $penjualan, ProdukRepository $produk)
     {
         $this->middleware('auth');
         $this->middleware('hak_akses');
         $this->profil = $profil;
         $this->penjualan = $penjualan;
+        $this->produk = $produk;
         view()->share(['title' => 'Penjualan']);
         $this->breadcrumbs = array(
             ['url' => null, 'caption' => 'Lemon Kasir', 'parameters' => null],
@@ -101,5 +103,39 @@ class PenjualanKasirController extends Controller
         $detail = $this->penjualan->delete_detail($request->input('id'));
         $detail->total_harga = $detail->total_harga;
         return $detail;
+    }
+
+    public function fake_data()
+    {
+        $profil = $this->profil->find(Auth::user()->user_profil->profil_id);
+        $list_produk_id = $this->produk->search(new Request())->pluck('id')->toArray();
+
+        for ($i = 1; $i <= date('t'); $i++) {
+            $tanggal = date('Y-m-d', strtotime(date('Y-m-') . $i));
+            for ($p = 1; $p <= rand(10, 20); $p++) {
+                $no_penjualan = $this->penjualan->nomor_otomatis();
+                $reqP = new Request(['profil_id' => $profil->id, 'no_penjualan' => $no_penjualan]);
+                $penjualan = $this->penjualan->save($reqP);
+                $total_penjualan = 0;
+                for ($d = 1; $d <= rand(3, 10); $d++) {
+                    $produk_id = $list_produk_id[array_rand($list_produk_id)];
+                    $reqD = new Request(
+                        [
+                            'penjualan_id' => $penjualan->id,
+                            'produk_id' => $produk_id,
+                            'jumlah' => rand(1, 3),
+                            'harga' => $this->produk->find($produk_id)->harga
+                        ]
+                    );
+                    $detail = $this->penjualan->save_detail($reqD);
+                    $total_penjualan += $detail->total_harga;
+                }
+                $penjualan->is_bayar = 1;
+                $penjualan->total = $total_penjualan;
+                $penjualan->dibayar = $total_penjualan;
+                $penjualan->tanggal_waktu_dibayar = date('Y-m-d H:i:s', strtotime($tanggal . ' 07:00:00'));
+                $penjualan->save();
+            }
+        }
     }
 }
